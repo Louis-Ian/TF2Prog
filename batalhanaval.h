4544dc098355c2dbc075475f22dcdcbc591215cd
HMSS <heliowp11@gmail.com>
2017-11-15 22:32:45 -0300

    Add files via upload

---
+++ batalhanaval.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct celula{
+	int linha;
+	int coluna;
+	int existe;
+	struct celula* dir;
+	struct celula* esq;
+	struct celula* top;
+	struct celula* bot; 
+};
+
+void inicializar(struct celula***){}
+void inserir(struct celula** ){}
+void criar(struct celula***){}
+void mapear(struct celula***){}
+void printmap(struct celula***){}
+int jogada(int ,int ){}
