1720f2863b5788f3ad36e8b26247645123268f09
HMSS <heliowp11@gmail.com>
2017-11-19 20:50:36 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "batalhanaval.h"
+
+
+int main(){
+	struct celula** mapa;
+	inicializar(&mapa);
+	criar(&mapa);
+	mapear(&mapa);
+	printmap(&mapa);
+	return 0;
+}
